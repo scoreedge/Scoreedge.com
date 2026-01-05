6d173361904ba754535cb6d792cbf4db8b065279
scoreedge <scoreedgeofficial@gmail.com>
2026-01-05 15:30:56 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+// Hamburger Menu Toggle
+const hamburger = document.getElementById('hamburger');
+const navMenu = document.getElementById('nav-menu');
+
+hamburger.addEventListener('click', () => {
+  navMenu.classList.toggle('active');
+});
